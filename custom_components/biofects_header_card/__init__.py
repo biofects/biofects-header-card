4376d3e34f9ce1066a70f32bfd6c756c49ca7615
tfam <biofects@gmail.com>
2024-10-31 12:37:47 -0500

    update init

--- custom_components/biofects_header_card/__init__.py
+++ custom_components/biofects_header_card/__init__.py
@@ -1,18 +1,7 @@
 """Biofects Header Card Component."""
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
-from homeassistant.helpers.discovery import load_platform
-
-from .const import DOMAIN
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the Biofects Header Card component."""
-    
-    # Setup sensor platform
-    hass.async_create_task(
-        hass.helpers.discovery.async_load_platform(
-            'sensor', DOMAIN, {}, config
-        )
-    )
-    
     return True
